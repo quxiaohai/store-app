60415154e5fd9edce61593125cd9b94bf2235353
quxiaohai <18927440520@163.com>
2026-06-11 15:20:59 +0800

    更新代码

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -3,7 +3,7 @@ import { ref } from 'vue'
 import viteLogo from 'assets/vite.svg'
 import heroImg from 'assets/hero.png'
 import vueLogo from 'assets/vue.svg'
-
+import './style.css'
 const count = ref(0)
 </script>
 
